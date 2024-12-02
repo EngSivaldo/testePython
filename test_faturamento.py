34b747c05205cb4a256c894fd057f36d51d29637
sivaldo vieira <sivaldovieiradealmeida@gmail.com>
2024-12-02 11:49:34 -0300

    realizando teste com arquivo individualmente

---
+++ test_faturamento.py
@@ -0,0 +1,6 @@
+from main import calcular_faturamento
+
+
+
+def test_calcular_faturamento():
+  assert type(calcular_faturamento()) == int
--- test_main.py
+++ test_main.py
@@ -3,4 +3,8 @@ from main import calcular_lucro, calcular_faturamento
 
 def test_calcular_lucro():
   faturamento = calcular_faturamento()
-  assert calcular_lucro(faturamento, 5000) > 0
+  assert calcular_lucro(faturamento, 500) > 0
+
+
+# def test_calcular_faturamento():
+#   assert type(calcular_faturamento()) == int
